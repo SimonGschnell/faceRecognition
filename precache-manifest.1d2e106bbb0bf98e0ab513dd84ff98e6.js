f9538c2a633f8e33d3449777a4fd036f9595b9eb
Simon Gschnell <Simon.Gschnell@stud-inf.unibz.it>
2020-04-19 17:16:56 +0200

    Updates

---
+++ precache-manifest.1d2e106bbb0bf98e0ab513dd84ff98e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8fbe75185433d5942efb73c3acc80f",
+    "url": "/faceRecognize/index.html"
+  },
+  {
+    "revision": "0e5ea4531f200c9611d0",
+    "url": "/faceRecognize/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c90fc00d68d4b1409205",
+    "url": "/faceRecognize/static/css/main.a294cdab.chunk.css"
+  },
+  {
+    "revision": "0e5ea4531f200c9611d0",
+    "url": "/faceRecognize/static/js/2.6e484f2c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/faceRecognize/static/js/2.6e484f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90fc00d68d4b1409205",
+    "url": "/faceRecognize/static/js/main.8afe2a6a.chunk.js"
+  },
+  {
+    "revision": "57b6d4a4584b6951615b",
+    "url": "/faceRecognize/static/js/runtime-main.6af1a981.js"
+  }
+]);
